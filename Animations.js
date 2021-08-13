386b5e7e3db0614be0c43647d509945370a2925f
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-08-13 06:00:36 -0700

    Create Animations.js

---
+++ Animations.js
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>ANIMATION</title>
+<style>
+#container
+ {
+   width:200px;
+   height:200px;
+   background:green;
+   position:relative;
+}
+
+#box
+{
+	width:50px;
+	height:50px;
+	background:red;
+	position:absolute;
+}
+
+</style>
+</body>
+<body>
+<div id="container">
+<div id="box"></div></div>
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
